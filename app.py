93ddc7ee51d7c78e9d9398584002ad396f201102
Drzezo55 <158335353+Drzezo55@users.noreply.github.com>
2025-02-13 21:33:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from sklearn.preprocessing import StandardScaler
 
 # Load the model and mappings
-model = joblib.load('C:/Users/ascom/Desktop/new paid/xg_reg.pkl')
+model = joblib.load('xg_reg.pkl')
 # Mapping dictionaries
 Sex_mapping = {'Male': 1, 'Female': 0}
 race_mapping = {
