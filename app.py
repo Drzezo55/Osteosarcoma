debe9b49d1efe4bd2758a6009c76deb3e0e78c26
Drzezo55 <158335353+Drzezo55@users.noreply.github.com>
2025-02-13 11:01:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ Chemotherapy_mapping= {'No': 0, 'Yes': 1}
 
 
 # Streamlit App
-st.image('download.jfif', use_column_width=True)
+st.image('download.jfif', use_container_width =True)
 st.title("Survival prediction of Osteosarcoma")
 Age = st.slider("Age (years)", min_value=0, max_value=100, value=50, step=1)
 Sex = st.radio("Sex", ['Male', 'Female'])
